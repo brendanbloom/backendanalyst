858a7b40a7ba558848f5f78dd2c7c903a6f31afe
brendanbloom <136516241+brendanbloom@users.noreply.github.com>
2023-06-13 17:58:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Add this line to enable CORS for all routes
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
